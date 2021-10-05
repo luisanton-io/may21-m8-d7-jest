0090e920480d57d4bc6fe2a1670d41141438939c
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-10-05 12:59:28 +0100

    npm i

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import app from "./server.js"
-import mongoose from "mongoose"
+import app from "./server.js";
+import mongoose from "mongoose";
 
-const port = process.env.PORT || 3001
-const mongoUrl = process.env.MONGO_URL
+const port = process.env.PORT || 3001;
+const mongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("🍀 Connected to MongoDB")
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("🍀 Connected to MongoDB");
     app.listen(port, () => {
-        console.log("🤘 Server listening on port " + port)
-    })
-})
+      console.log("🤘 Server listening on port " + port);
+    });
+  });
